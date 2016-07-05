97eb2288cca85ad7e3cef922fc50ab58518cf69c
Jaydeep <jaydp17@gmail.com>
2016-07-06 04:35:46 +0530

    import lodash and filter array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 'use strict';
 
-const arr = [0, 1, 2].map(v => v + 1);
+import _ from 'lodash'
 
+let arr = [0, 1, 2].map(v => v + 1);
+
+arr = _.filter(arr, i => i > 1);
 
 alert(`hey there, ${arr}`);
